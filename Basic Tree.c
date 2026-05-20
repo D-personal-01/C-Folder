7690dfd2c9a8e40979fe2cb67b009d587cdc482b
Daksh Joshi <joshidaksh085@gmail.com>
2026-05-20 17:50:03 +0530

    Basic tree

---
+++ Basic Tree.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+
+
+    struct Node* root = createNode(1);
+
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    
+    root->left->left->right= createNode(6);
+    root->left->right->left = createNode(7);
+    root->left->right->right = createNode(8);
+
+    printf("Inorder Traversal: ");
+    inorder(root);
+
+    return 0;
+}
